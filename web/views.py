29726c6f3a025539978ded6f7b312f3a406e50ee
TK-8888 <10930294@dcsh.tp.edu.tw>
2020-12-24 11:22:07 +0800

    修改刪除留言

--- web/views.py
+++ web/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Message
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class MessageList(ListView):
     model = Message
@@ -16,6 +17,6 @@ class MessageCreate(CreateView):
     fields = '__all__'
     success_url = reverse_lazy('msg_list')
     
-class MessageDelete(DeleteView):
+class MessageDelete(LoginRequiredMixin, DeleteView):
     model = Message
     success_url = reverse_lazy('msg_list') 
